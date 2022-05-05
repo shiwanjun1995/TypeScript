1325cc3002130baf78b090e49d80d4bc74ee2db1
wanjun.shi <wanjun.shi@transsion.com>
2022-05-05 12:54:38 +0800

    add: 显示类型

--- 01-ts-basic/hello.js
+++ 01-ts-basic/hello.js
@@ -1,6 +1,10 @@
+"use strict";
 // // 你好，世界
 // console.log('Hello World');
 function greet(person, date) {
     console.log(`Hello ${person}, today is ${date}.`);
 }
-greet('小猪', '2022-05-05');
+greet('小猪', new Date());
+// let msg = 'hello there!'
+// msg = 'hello world'
+// msg = 100
--- 01-ts-basic/hello.ts
+++ 01-ts-basic/hello.ts
@@ -1,8 +1,12 @@
 // // 你好，世界
 // console.log('Hello World');
 
-function greet(person, date) {
+function greet(person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date}.`)
 }
 
-greet('小猪', '2022-05-05')
+greet('小猪', new Date())
+
+// let msg = 'hello there!'
+// msg = 'hello world'
+// msg = 100
