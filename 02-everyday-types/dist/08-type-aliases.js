89299b04074ba1a11157608afa29df5375400d96
wanjun.shi <wanjun.shi@transsion.com>
2022-05-05 22:54:25 +0800

    add: 变量名不能重名，可以使用接口来定义对象类型

--- 02-everyday-types/dist/08-type-aliases.js
+++ 02-everyday-types/dist/08-type-aliases.js
@@ -1,16 +1,23 @@
 "use strict";
-function printPoint(pt) {
-}
-printPoint({
-    x: 100,
-    y: 200
-});
-function printID(id) {
-}
-printID(100);
-printID('hello');
-function sanitizedInput(str) {
-    return str.slice(0, 2);
-}
-let userInput = sanitizedInput('hello');
-userInput = 'new input';
+// type Point = {
+//   x: number,
+//   y: number
+// }
+// function printPoint(pt: Point) {
+// }
+// printPoint({
+//   x: 100,
+//   y: 200
+// })
+// // type 定义的只是一个类型，而不是一个变量
+// type ID = number | string
+// function printID(id: ID) {
+// }
+// printID(100)
+// printID('hello')
+// type UserInputSanitizedString = string
+// function sanitizedInput(str: string): UserInputSanitizedString {
+//   return str.slice(0, 2)
+// }
+// let userInput = sanitizedInput('hello')
+// userInput = 'new input'
---
+++ 02-everyday-types/dist/09-interface.js
@@ -0,0 +1,52 @@
+"use strict";
+function printPoint(pt) {
+}
+printPoint({
+    x: 100,
+    y: 100
+});
+// 类型别名和接口是很类似的，几乎所有可以使用 interface 定义的这种类型都可以通过 type 这种来定义
+// // 扩展接口
+// interface Animal {
+//   name: string
+// }
+// // 定义了一个新的属性
+// interface Bear extends Animal {
+//   honney: Boolean
+// }
+// // 声明一个bear 的实例
+// const bear: Bear = {
+//   name: 'winie',
+//   honney: true
+// }
+// console.log(bear.name);
+// console.log(bear.honney);
+// // 交叉扩展
+// type Animal = {
+//   name: string
+// }
+// type Bear = Animal & {
+//   honney: Boolean
+// }
+// const bear: Bear = {
+//   name: 'winie',
+//   honney: true
+// }
+// // 向现有的类型添加字段 interface
+// interface MyWindow {
+//   count: number
+// }
+// interface MyWindow {
+//   title: string
+// }
+// const w: MyWindow = {
+//   title: 'hello ts',
+//   count: 100
+// }
+// 类型创建以后不能进行扩展 type
+// type MyWindow = {
+//   title: string
+// }
+// type MyWindow = {
+//   count: number
+// }
