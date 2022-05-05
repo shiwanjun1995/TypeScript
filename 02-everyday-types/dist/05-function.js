bf0d363253622d344d1a9d9f9ec7cb562ff75485
wanjun.shi <wanjun.shi@transsion.com>
2022-05-05 21:44:07 +0800

    add: 函数会自动根据上下文类型推导指定类型

---
+++ 02-everyday-types/dist/05-function.js
@@ -0,0 +1,17 @@
+"use strict";
+function greet(name) {
+    console.log('Hello，' + name.toUpperCase() + '!!');
+}
+greet('大猪');
+function getFavoriteNumber() {
+    // 通常不需要定义函数的返回类型注释，ts会自动推断
+    return 20;
+}
+// 当匿名函数可以确定它如何被调动的地方的时候，这个函数的参数会自动地指定类型
+const names = ['小猪', '大猪'];
+names.forEach(function (s) {
+    console.log(s.toUpperCase());
+});
+names.forEach((s) => {
+    console.log(s.toUpperCase());
+});
